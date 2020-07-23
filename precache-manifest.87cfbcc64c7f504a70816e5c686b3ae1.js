e2c0ae26ec2b83de04b9a8377d766369c77720d9
dashixiong <2788347834@qq.com>
2020-07-23 22:44:35 +0800

    deploy

---
+++ precache-manifest.87cfbcc64c7f504a70816e5c686b3ae1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89484c4b00d3bad2911a",
+    "url": "/css/app.7609a8ed.css"
+  },
+  {
+    "revision": "210d7d0d94fd9a636898",
+    "url": "/css/chunk-vendors.e7de7990.css"
+  },
+  {
+    "revision": "44f16f6d2cfb2caeba03d840ab24120d",
+    "url": "/img/Add.44f16f6d.svg"
+  },
+  {
+    "revision": "b1f7ffeb8d3893c4fce004e85f8c5df4",
+    "url": "/img/Delete.b1f7ffeb.svg"
+  },
+  {
+    "revision": "2a8811288c07e4dd5cb106da572c5c2e",
+    "url": "/img/Detail.2a881128.svg"
+  },
+  {
+    "revision": "c01ef212a6f128937f669ce207d8a1b2",
+    "url": "/img/Label.c01ef212.svg"
+  },
+  {
+    "revision": "daf8e2b19802ee9a91866223a07a8fee",
+    "url": "/img/Money.daf8e2b1.svg"
+  },
+  {
+    "revision": "8dfa7cf784abd29cb0ab538658c5a668",
+    "url": "/img/Statistics.8dfa7cf7.svg"
+  },
+  {
+    "revision": "32bdda8aa5a68539b494d6bf9e0c42ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89484c4b00d3bad2911a",
+    "url": "/js/app.785a9962.js"
+  },
+  {
+    "revision": "210d7d0d94fd9a636898",
+    "url": "/js/chunk-vendors.442e520d.js"
+  },
+  {
+    "revision": "c09783398bfb82f81cfc539621c3b018",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
